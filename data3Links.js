842dccd97855f0957258a4a85ee885a5b22c07e0
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-03 04:37:26 +0000

    commit pdf

--- data3Links.js
+++ data3Links.js
@@ -1,12 +1,12 @@
 // data3Links.js - Links Dataset
 var data3Links = `ID,Categry,Subject,Remark,Link
-011,Notes,patient innovation platform of portugal,html,patientinnovation.htm
+011,Call for paper,Notice & Topics,pdf,callforpaperofnyaykawaj2082.pdf
 
 `;
 // Dataset metadata
 var data3LinksInfo = {
     name: "Notes",
-    description: "Non url links,repo documents links",
+    description: "LINKS",
     emoji: "🔗",
     columns: 5,
     primaryKey: "ID"
