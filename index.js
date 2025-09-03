a967c53f82f15e62a26e9dc3325e6b7a7b6d8280
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-03 13:35:55 +0545

    Update index.js

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ function loadUserPreferences() {
         // Note: Since localStorage is not available in this environment,
         // we'll use a simple in-memory preference system
         window.userPreferences = {
-            lastUsedDataset: 'data3Links',
+            lastUsedDataset: 'data4Status',
             searchHistory: [],
             preferredTheme: 'default',
             accessibilityMode: false
@@ -403,3 +403,4 @@ style.textContent = `
 `;
 
 document.head.appendChild(style);
+
